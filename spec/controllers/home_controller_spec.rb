5fd7e4465c06b793bdd76f51c3e8c72f67feb58a
Dan Kissell <dkissell@gmail.com>
2011-08-16 01:11:37 -0400

    Added content to Tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "should serve 'home' from index.html.erb" do
     get 'index'
     response.should be_success
   end
  end
 
   describe "GET 'about'" do
-    it "should render 'about'" do
+    it "should serve 'about'" do
     get 'about'
     response.should be_success
   end
